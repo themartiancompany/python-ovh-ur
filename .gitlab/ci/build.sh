8a2e6528f8e98c5390bdb093b64507453b434682
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-04-10 18:04:53 +0000

    hur

--- .gitlab/ci/build.sh
+++ .gitlab/ci/build.sh
@@ -2,15 +2,17 @@
 
 # SPDX-License-Identifier: AGPL-3.0
 
-#    ----------------------------------------------------------------------
-#    Copyright © 2022, 2023, 2024, 2025  Pellegrino Prevete
+#    ---------------------------------------------------------------
+#    Copyright © 2022, 2023, 2024, 2025, 2026
+#              Pellegrino Prevete
 #
 #    All rights reserved
-#    ----------------------------------------------------------------------
+#    ---------------------------------------------------------------
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    This program is free software: you can redistribute it
+#    and/or modify it under the terms of the GNU Affero
+#    General Public License as published by the Free Software
+#    Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,14 +20,22 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General
+#    Public License along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
 
 
 # This script is run within a virtual environment to build
-#  pakcage
+# the package
 # $1: platform
 # $2: architecture
+# $3: namespace
+# $4: package
+# $5: (optional) project-id
+# $6: (optional) commit
+# $7: (optional) tag
+# $8: (optional) ci-job-token
+# $9: (optional) package-registry-url
 
 set \
   -euo \
@@ -39,10 +49,16 @@ _gur_mini() {
     _ns="${1}" \
     _pkg="${2}" \
     _release="${3}" \
+    _depends_skip \
+    _pacman_opts=() \
     _api \
     _url \
     _msg=() \
     _sig
+  _depends_skip="y"
+  if (( 3 < "${#}" )); then
+    _depends_skip="${4}"
+  fi
   _msg=(
     "Downloading '${_pkg}'"
     "binary CI release"
@@ -53,16 +69,26 @@ _gur_mini() {
     "${_msg[*]}"
   _gl_dl_retrieve \
     "https://gitlab.com/api/v4/projects/${_ns}%2F${_pkg}-ur"
-  _project_id="$( \
+  _project_id="$(
     cat \
       "${HOME}/${_ns}%2F${_pkg}-ur" | \
       jq \
         '.id')"
+  if [[ "${_project_id}" == "null" ]]; then
+    _msg=(
+      "The project '${_pkg}-ur' does not exist"
+      "in namespace '${_ns}'."
+    )
+    echo \
+      "${_msg[*]}"
+    return \
+      1
+  fi
   _api="https://gitlab.com/api/v4"
   _url="${_api}/projects/${_project_id}/releases"
   _gl_dl_retrieve \
     "${_url}"
-  _urls=( $( \
+  _urls=( $(
     cat \
       "${HOME}/releases" | \
       jq \
@@ -72,7 +98,7 @@ _gur_mini() {
           '.direct_asset_url')
   )
   for _url in "${_urls[@]}"; do
-    _file="$( \
+    _file="$(
       basename \
         "${_url}")"
     _output_file="$(pwd)/${_file}"
@@ -87,9 +113,19 @@ _gur_mini() {
   rm \
     -rf \
     "${HOME}/"*".pkg.tar.xz.sig"
+  _pacman_opts+=(
+    -U
+  )
+  if [[ "${_depends_skip}" == "y" ]]; then
+    _pacman_opts+=(
+      -dd
+    )
+  fi
+  _pacman_opts+=(
+    --noconfirm
+  )
   pacman \
-    -Udd \
-    --noconfirm \
+    "${_pacman_opts[@]}" \
     "${HOME}/"*".pkg.tar.xz"
 }
 
@@ -114,7 +150,7 @@ _fur_mini() {
     --single-branch
     --depth=1
   )
-  _tmp_dir="$( \
+  _tmp_dir="$(
     mktemp \
       "${_mktemp_opts[@]}")"
   git \
@@ -134,11 +170,81 @@ _fur_mini() {
     "${_tmp_dir}/fur"
 }
 
+_check_tag_latest() {
+  local \
+    _pkgname="${1}" \
+    _msg=() \
+    _tag \
+    _tag_build \
+    _tag_current \
+    _tag_recipe \
+    _repo_dir
+  _repo_dir="/home/user/${_pkgname}"
+  git \
+    config \
+      --global \
+      --add \
+        "safe.directory" \
+        "${_repo_dir}"
+  _tag="$(
+    git \
+      -C \
+        "${_repo_dir}" \
+      tag |
+      sort \
+        -V |
+        tail \
+          -n \
+            1)"
+  _tag_build="${tag}"
+  _tag_current="${_tag_build}"
+  _tag_recipe="$(
+    recipe-get \
+      "${_repo_dir}/PKGBUILD" \
+      "pkgver" || \
+      true)-$(
+        recipe-get \
+          "${_repo_dir}/PKGBUILD" \
+          "pkgrel")"
+  _tag_current="${_tag_recipe}"
+  if [[ "${_tag}" != "${_tag_current}" ]]; then
+    _msg=(
+      "Current build tag '${_tag_current}',"
+      "latest tag '${_tag}'."
+    )
+    echo \
+      "${_msg[*]}"
+    exit \
+      0
+  fi
+}
+
 _requirements() {
   local \
     _fur_mini_opts=() \
     _fur_opts=() \
-    _pkgname
+    _pkgname \
+    _pkgver \
+    _commit \
+    _docs_commit \
+    _git_http \
+    _evm_chains_release_latest \
+    _evm_chains_explorers_release_latest \
+    _evm_chains_info_release_latest \
+    _evm_contracts_abi_get_release_latest \
+    _evm_contracts_tools_release_latest \
+    _evm_gnupg_release_latest \
+    _evm_wallet_release_latest \
+    _fur_release_latest \
+    _fur_release_public \
+    _gl_dl_release_latest \
+    _gur_release_latest \
+    _libcrash_bash_release_latest \
+    _libcrash_js_release_latest \
+    _node_run_release_latest \
+    _python_asyncio_throttle_release_latest \
+    _python_aioetherscan_release_latest \
+    _python_aiohttp_retry_release_latest
   _pkgname="${pkg%-ur}"
   _fur_mini_opts+=(
     "${platform}"
@@ -149,12 +255,12 @@ _requirements() {
   _fur_mini \
     "fur" \
     "${_fur_mini_opts[@]}"
-  _fur_release="0.0.1.1.1.1.1.1.1.1.1.1.1"
-  _fur_opts+=(
-    -v
-    -p
-      "pacman"
-  )
+  _fur_release_public="0.0.1.1.1.1.1.1.1.1.1.1.1"
+  # _fur_opts+=(
+  #   -v
+  #   -p
+  #     "pacman"
+  # )
   # pacman \
   #   -S \
   #   --noconfirm \
@@ -163,25 +269,175 @@ _requirements() {
     "${ns}" \
     "reallymakepkg" \
     "1.2.5-1" || \
-  fur \
-    "${_fur_opts[@]}" \
-    "reallymakepkg"
-  # ohoh
-  recipe-get \
-    -v \
-    "/home/user/${_pkgname}/PKGBUILD" \
-    "_commit"
-  _commit="$( \
+  true
+  _check_tag_latest \
+    "${_pkgname}"
+  _evm_chains_release_latest="20250816-6"
+  _evm_chains_explorers_release_latest="0.0.0.0.0.0.0.0.0.0.1.1.1-5"
+  _evm_chains_info_release_latest="0.0.0.0.0.0.0.0.0.0.1.1.1.1.1.1.1-8"
+  _evm_contracts_abi_get_release_latest="0.0.0.0.0.0.0.0.0.0.0.1.1.1.1.1.1-3"
+  _evm_contract_tools_release_latest="0.0.0.0.0.0.0.0.1.1.1.1.1-4"
+  _evm_gnupg_release_latest="0.0.0.0.0.0.0.0.1.1.1.1.1.1-5"
+  _evm_wallet_release_latest="0.0.0.0.0.0.0.0.0.0.1.1.1-7"
+  _fur_release_latest="1.0.0.0.0.0.0.0.0.0.1.1-2"
+  _gl_dl_release_latest="0.0.0.0.0.0.0.0.0.0.0.0.1.1.1.1.1.1-5"
+  _gur_release_latest="0.0.0.0.0.0.0.0.0.0.0.0.0.0.1.1-9"
+  _key_gen_release_latest="0.0.0.0.0.0.0.0.0.0.0.1-5"
+  _libcrash_bash_release_latest="0.0.0.0.0.1.1.1.1.1-11"
+  _libcrash_js_release_latest="0.1.69-25"
+  _libevm_release_latest="0.0.0.0.0.0.0.0.1.1.1.1.1.1.1-4"
+  _node_run_release_latest="0.0.0.0.0.0.0.0.1-10"
+  _python_aiohttp_retry_release_latest="2.8.3-7"
+  _python_aioetherscan_release_latest="0.9.6.1-9"
+  _python_asyncio_throttle_release_latest="1.0.2-9"
+  _ucantellme_release_latest="0.0.0.0.0.0.0.0.0.0.0.0.0.1.1.1-3"
+  _gur_mini \
+    "${ns}" \
+    "libcrash-bash" \
+    "${_libcrash_bash_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "libcrash-js" \
+    "${_libcrash_js_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "node-run" \
+    "${_node_run_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "gl-dl" \
+    "${_gl_dl_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "ucantellme" \
+    "${_ucantellme_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "key-gen" \
+    "${_key_gen_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-chains" \
+    "${_evm_chains_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-chains-explorers" \
+    "${_evm_chains_explorers_release_latest}" \
+    "n" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-chains-info" \
+    "${_evm_chains_info_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "libevm" \
+    "${_libevm_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "python-asyncio-throttle" \
+    "${_python_asyncio_throttle_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "python-aiohttp-retry" \
+    "${_python_aiohttp_retry_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "python-aioetherscan" \
+    "${_python_aioetherscan_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-contracts-abi-get" \
+    "${_evm_contracts_abi_get_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-wallet" \
+    "${_evm_wallet_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-contracts-tools" \
+    "${_evm_contract_tools_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "evm-gnupg" \
+    "${_evm_gnupg_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "gur" \
+    "${_gur_release_latest}" || \
+  true
+  _gur_mini \
+    "${ns}" \
+    "fur" \
+    "${_fur_release_latest}" || \
+  true
+  pacman \
+    -S \
+    --noconfirm \
+    "tree" \
+    "github-cli" \
+    "unzip"
+  # _fur_opts+=(
+  #   -l
+  #     "bur"
+  #   -m
+  #     "gitlab"
+  # )
+  # fur \
+  #   "${_fur_opts[@]}" \
+  #   "gur"
+  # _fur_opts+=(
+  #   -t
+  #     "ci"
+  # )
+  # fur \
+  #   "${_fur_opts[@]}" \
+  #   "reallymakepkg"
+  _git_http="$(
     recipe-get \
       "/home/user/${_pkgname}/PKGBUILD" \
-      "_commit")"
-  _gl_dl_mini \
-    "${ns}" \
-    "${_pkgname}" \
-    "${_commit}"
-  # cp \
-  #   "${HOME}/${_pkgname}-${_commit}.tar.gz" \
-  #   "/home/user/${_pkgname}"
+      "_git_http" || \
+      true)"
+  # ohoh
+  if [[ "${_git_http}" == "gitlab" ]]; then
+    _commit="$(
+      recipe-get \
+        "/home/user/${_pkgname}/PKGBUILD" \
+        "_commit")"
+    _gl_dl_mini \
+      "${ns}" \
+      "${_pkgname}" \
+      "${_commit}"
+    cp \
+      "${HOME}/${_pkgname}-${_commit}.tar.gz" \
+      "/home/user/${_pkgname}"
+  else
+    echo \
+      "Direct download in the PKGBUILD"
+  fi
 }
 
 _build() {
@@ -195,41 +451,73 @@ _build() {
     _depend_pkgver \
     _depend_target \
     _home \
+    _oldpwd \
+    _node_available \
+    _pacman_opts=() \
     _pkgbuild \
     _pkgname \
     _resolve_flag \
-    _work_dir
+    _work_dir \
+    _separators=() \
+    _msg=()
+  declare \
+    -A \
+    _makedepends_set
+  _separators=(
+    "<"
+    ">"
+    "<="
+    ">="
+    "="
+  )
   _home="/home/user"
   _pkgname="${pkg%-ur}"
-  _work_dir="${_home}/ramdisk/${_pkgname}-build"
+  _work_dir="${_home}/${_pkgname}-build"
   _pkgbuild="${_home}/${_pkgname}/PKGBUILD"
-  mount  | \
-    grep "${_home}/ramdisk"
+  _ramdisk_enabled="$(
+    mount |
+      grep \
+        "${_home}/ramdisk" || \
+      true)"
+  if [[ "${_ramdisk_enabled}" != "" ]]; then
+    _work_dir="${_home}/ramdisk/${_pkgname}-build"
+  fi
   _reallymakepkg_opts+=(
     -v
     -w
       "${_work_dir}"
   )
   _makepkg_opts+=(
-    -df
+    -d
+    -f
     --nocheck
   )
-  for _depend in $(recipe-get \
-                     "${_pkgbuild}" \
-        "makedepends"); do
+  if [[ "${ns}" != "themartiancompany" ]]; then
+    _evmfs="$(
+      recipe-get \
+        "/home/user/${_pkgname}/PKGBUILD" \
+        "_evmfs")"
+    if [[ "${_evmfs}" == "false" ]]; then
+      _makepkg_opts+=(
+        --skipinteg
+      )
+    fi
+  fi
+  for _depend \
+    in $(recipe-get \
+           "${_pkgbuild}" \
+           "makedepends"); do
     _resolve_flag="false"
     _depend_target="${_depend}"
-    if [[ "${_depend}" == *"<"* ]]; then
-      _depend_name="${_depend%<*}"
-      _depend_pkgver="${_depend#${_depend_name}}"
-      _resolve_flag="true"
-      _depend_target="${_depend_name}"
-    elif [[ "${_depend}" == *"="* ]]; then
-      _depend_name="${_depend%=*}"
-      _depend_pkgver="${_depend#=${_depend_name}}"
-      _resolve_flag="true"
-      _depend_target="${_depend_name}"
-    fi
+    for _sep in "${_separators[@]}"; do
+      if [[ "${_depend}" == *"${_sep}"* ]]; then
+        _depend_name="${_depend%${_sep}*}"
+        _depend_pkgver="${_depend#${_depend_name}}"
+        _resolve_flag="true"
+        _depend_target="${_depend_name}"
+        break
+      fi
+    done
     if [[ "${_resolve_flag}" == "true" ]]; then
       _msg=(
         "It is requested version"
@@ -241,20 +529,130 @@ _build() {
       echo \
         "${_msg[*]}"
     fi
-    _makedepends+=(
-      "${_depend_target}"
-    )
+    _makedepends_set["${_depend_target}"]="1"
   done
-  echo uffa &>2
-  pacman \
-    -S \
-    --noconfirm \
-      "${_makedepends[@]}" || \
+  _makedepends=(
+      "${!_makedepends_set[@]}"
+  )
+  _fur_opts+=(
+    -v
+    -p
+      "pacman"
+  )
+  _msg=(
+    "Found makedepends"
+    "'${_makedepends[*]}'."
+  )
+  echo \
+    "${_msg[*]}"
+  for _depend in "${_makedepends[@]}"; do
+    _msg=(
+      "Installing makedepend"
+      "'${_depend}'"
+      "with pacman."
+    )
+    echo \
+      "${_msg[*]}"
+    _pacman_opts=(
+      -S
+      --noconfirm
+    )
+    if [[ "${_depend}" == "nodejs-lts"* ]]; then
+      _node_available="$(
+        ( pacman \
+            -Q \
+            "nodejs" \
+            2>"/dev/null" ) |
+          grep \
+            "^Name\b" |
+          awk \
+            -F \
+              ":" \
+            '{print $2}' \
+          2>/dev/null)"
+      if [[ "${_node_available}" == "nodejs" ]]; then
+        _pacman_opts+=(
+          --ask
+            4
+          -dd
+        )
+        _msg=(
+          "A specific version of Node.js"
+          "LTS is required as a dependency."
+          "That's ill-posed, as if a specific"
+          "package version does require a specific"
+          "version of Node then it should request"
+          "that version and not just simply"
+          "a 'latest - N' version because"
+          "that's not a version it's a tag."
+          "So more or less somebody should produce"
+          "not even 'nodejs-lts-jod' but simply"
+          "'nodejsXY' and require it in the package."
+        )
+        echo \
+          "${_msg[*]}"
+      elif [[ "${_node_available}" == "nodejs-lts"* ]]; then
+        _msg=(
+          "Node.js LTS installed."
+        )
+        echo \
+          "${_msg[*]}"
+      fi
+    fi
+    pacman \
+      -S \
+      --noconfirm \
+        "${_depend}" || \
+      true
+    _msg=(
+      "Installing makedepends"
+      "'${_depend}' with"
+      "fur."
+    )
+    echo \
+      "${_msg[*]}"
+    fur \
+      "${_fur_opts[@]}" \
+      -t \
+        "ci" \
+      -n \
+        "${ns}" \
+      "${_depend}" || \
+    fur \
+      "${_fur_opts[@]}" \
+      -t \
+        "tree" \
+      -m \
+        "gitlab" \
+      -l \
+        "bur" \
+      "${_depend}" || \
+    fur \
+      "${_fur_opts[@]}" \
+      -t \
+        "tree" \
+      -m \
+        "github" \
+      -l \
+        "fur" \
+      "${_depend}" || \
     true
-  echo "wattafacc" &>2
+  done
   _cmd+=(
     "cd"
       "${_home}/${_pkgname}" "&&"
+  )
+  _git_http="$(
+    recipe-get \
+      "/home/user/${_pkgname}/PKGBUILD" \
+      "_git_http" || \
+      true)"
+  if [[ "${_git_http}" == "gitlab" ]]; then
+    _cmd+=(
+      "_ns='${ns}'"
+    )
+  fi
+  _cmd+=(
     "reallymakepkg"
       "${_reallymakepkg_opts[@]}"
       "--"
@@ -268,6 +666,7 @@ _build() {
   _something_built="false"
   for _file in "${_home}/${_pkgname}/"*".pkg.tar."*; do
     _something_built="true"
+    break
   done
   if [[ "${_something_built}" == "true" ]]; then
     pacman \
@@ -279,19 +678,29 @@ _build() {
       "Build failed, printing"
       "work directory content."
     )
+    echo \
+      "${_msg[*]}"
     tree \
-      -L 5 \
+      -L 2 \
+      "${_work_dir}"
+    _oldpwd="${PWD}"
+    cd \
       "${_work_dir}"
     tar \
       cJf \
       "build-directory.tar.xz" \
-      "${_work_dir}"
+      *
+    mv \
+      "build-directory.tar.xz" \
+      "${_oldpwd}"
+    cd \
+      "${_oldpwd}"
   fi
   for _file \
     in "${_home}/${_pkgname}/"*".pkg.tar."*; do
     mv \
       "${_file}" \
-      "dogeos-gnu-$( \
+      "dogeos-gnu-$(
         basename \
           "${_file}")"
   done
@@ -339,7 +748,7 @@ _gl_dl_retrieve() {
     _output_file \
     _msg=() \
     _token_missing
-  _output_file="${HOME}/$( \
+  _output_file="${HOME}/$(
     basename \
       "${_url#https://}")"
   _token_private="${HOME}/.config/gitlab.com/default.txt"
@@ -368,7 +777,7 @@ _gl_dl_retrieve() {
     exit \
       1
   fi
-  _token="PRIVATE-TOKEN: $( \
+  _token="PRIVATE-TOKEN: $(
     cat \
       "${_token_private}")"
   _curl_opts+=(
@@ -398,7 +807,7 @@ _mem_free_get() {
 }
 
 _show_config() {
-  _mem_free="$( \
+  _mem_free="$(
     _mem_free_get)"
   echo \
     "Free memory: '${_mem_free}'"
--- PKGBUILD
+++ PKGBUILD
@@ -34,23 +34,60 @@
 #   ebiadsu
 #     <ebiadsu@posteo.de>
 
+_evmfs_available="$(
+  command \
+    -v \
+    "evmfs" || \
+    true)"
+if [[ ! -v "_evmfs" ]]; then
+  if [[ "${_evmfs_available}" != "" ]]; then
+    _evmfs="true"
+  elif [[ "${_evmfs_available}" == "" ]]; then
+    _evmfs="false"
+  fi
+fi
+if [[ ! -v "_git" ]]; then
+  _git="false"
+fi
+if [[ ! -v "_git_service" ]]; then
+  _git_service="github"
+fi
 if [[ ! -v "_docs" ]]; then
   _docs="true"
 fi
+_pkg=ovh
+if [[ ! -v "_ns" ]]; then
+  _ns="themartiancompany"
+  # _ns="${_pkg}"
+fi
+if [[ ! -v "_archive_format" ]]; then
+  if [[ "${_git}" == "true" ]]; then
+    if [[ "${_evmfs}" == "true" ]]; then
+      _archive_format="bundle"
+    elif [[ "${_evmfs}" == "false" ]]; then
+      _archive_format="git"
+    fi
+  elif [[ "${_git}" == "false" ]]; then
+    if [[ "${_git_service}" == "github" ]]; then
+      _archive_format="zip"
+    elif [[ "${_git_service}" == "gitlab" ]]; then
+      _archive_format="tar.gz"
+    fi
+  fi
+fi
 _py="python"
-_pyver="$( \
+_pyver="$(
   "${_py}" \
-    -V | \
+    -V |
     awk \
       '{print $2}')"
 _pymajver="${_pyver%.*}"
 _pyminver="${_pymajver#*.}"
-_pynextver="${_pymajver%.*}.$(( \
+_pynextver="${_pymajver%.*}.$((
   ${_pyminver} + 1))"
-_pkg=ovh
 pkgbase="${_py}-${_pkg}"
 pkgname=(
-  "${_py}-${_pkg}"
+  "${pkgbase}"
 )
 if [[ "${_docs}" == "true" ]]; then
   pkgname+=(
@@ -58,18 +95,20 @@ if [[ "${_docs}" == "true" ]]; then
   )
 fi
 pkgver=1.1.2
+_commit="7532f33038f01777bf74a2ec57d6aa0666039542"
 pkgrel=3
 pkgdesc="Lightweight wrapper around OVH's APIs"
 arch=(
   'any'
 )
-_http="https://github.com"
-_ns="${_pkg}"
+_http="https://${_git_service}.com"
 url="${_http}/${_ns}/${pkgbase}"
 license=(
   'BSD'
 )
 depends=(
+  "${_py}>=${_pymajver}"
+  "${_py}<${_pynextver}"
   "${_py}-requests"
 )
 makedepends=(
@@ -84,13 +123,89 @@ if [[ "${_docs}" == "true" ]]; then
     "${_py}-sphinx"
   )
 fi
-_tarname="${pkgbase}-${pkgver}"
-_sum='b24a47bc37ffb14fee2d9525b4aa0b86eeb2aab24755fd6e74707c4e4d0b807a'
+if [[ "${_evmfs}" == "true" ]]; then
+  makedepends+=(
+    "evmfs"
+  )
+fi
+if [[ "${_git}" == "true" ]]; then
+  makedepends+=(
+    "git"
+  )
+fi
+_url="${url}"
+_tag="${_commit}"
+_tag_name="commit"
+#_tag="${pkgver}"
+# _tag_name="pkgver
+_tarname="${pkgbase}-${_tag}"
+_tarfile="${_tarname}.${_archive_format}"
+_release_sum='b24a47bc37ffb14fee2d9525b4aa0b86eeb2aab24755fd6e74707c4e4d0b807a'
+_release_sig_sum="9031637a3fdbd932db3cb1a976086ff890704c08429d26cd64f606459682a754"
 _uri="${url}/archive/v${pkgver}.tar.gz"
-source=(
-  "${_tarname}.tar.gz::${_uri}"
+_gitlab_sum="728ece7c8aea5504dd921850e40821768169a846dd5a825087ed5ade2636d91b"
+_gitlab_sig_sum="ab3de9fafca46637417c2aeab9e4aac41643c1ee61ec5ed958e4d23d2c91ac15"
+_github_sum="7a55511d466126a07ac33ac26b6fd3c82bef9ff5016d48dbb66d6522e9e0d489"
+_github_sig_sum="61bcb3eedd422bd48d6c19ae91b955ab4ade5b92f2a2dd1c7f34c34087ef59a3"
+if [[ "${_git_service}" == "gitlab" ]]; then
+  if [[ "" == "pkgver" ]]; then
+    _sum="${_release_sum}"
+    _sig_sum="${_release_sig_sum}"
+  fi
+  _sum="${_gitlab_sum}"
+  _sig_sum="${_gitlab_sig_sum}"
+  # Dvorak
+  _evmfs_ns="0x87003Bd6C074C713783df04f36517451fF34CBEf"
+elif [[ "${_git_service}" == "github" ]]; then
+  _sum="${_github_sum}"
+  _sig_sum="${_github_sig_sum}"
+  # Truocolo
+  _evmfs_ns="0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b"
+  # Dvorak
+  _evmfs_ns="0x87003Bd6C074C713783df04f36517451fF34CBEf"
+fi
+_evmfs_network="100"
+_evmfs_address="0x69470b18f8b8b5f92b48f6199dcb147b4be96571"
+_evmfs_dir="evmfs://${_evmfs_network}/${_evmfs_address}/${_evmfs_ns}"
+_evmfs_uri="${_evmfs_dir}/${_sum}"
+_evmfs_src="${_tarfile}::${_evmfs_uri}"
+_sig_uri="${_evmfs_dir}/${_sig_sum}"
+_sig_src="${_tarfile}.sig::${_sig_uri}"
+source=()
+sha256sums=()
+if [[ "${_evmfs}" == "true" ]]; then
+  if [[ "${_git}" == "false" ]]; then
+    _src="${_evmfs_src}"
+    source+=(
+      "${_sig_src}"
+    )
+    sha256sums+=(
+      "${_sig_sum}"
+    )
+  fi
+elif [[ "${_evmfs}" == "false" ]]; then
+  if [[ "${_git}" == true ]]; then
+    _src="${_tarname}::git+${_url}#${_tag_name}=${_tag}?signed"
+    _sum="SKIP"
+  elif [[ "${_git}" == false ]]; then
+    _uri=""
+    if [[ "${_git_service}" == "github" ]]; then
+      if [[ "${_tag_name}" == "commit" ]]; then
+        _uri="${_url}/archive/${_commit}.${_archive_format}"
+        _sum="${_github_sum}"
+      fi
+    elif [[ "${_git_service}" == "gitlab" ]]; then
+      if [[ "${_tag_name}" == "commit" ]]; then
+        _uri="${_url}/-/archive/${_tag}/${_tag}.${_archive_format}"
+      fi
+    fi
+    _src="${_tarfile}::${_uri}"
+  fi
+fi
+source+=(
+  "${_src}"
 )
-sha256sums=(
+sha256sums+=(
   "${_sum}"
 )
 
